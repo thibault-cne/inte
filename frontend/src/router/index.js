42c8b496ef63e1ef76ff492d83fa5e04379b80fc
Thib <thibault.cheneviere@telecomnancy.net>
2022-05-06 13:13:30 +0200

    [AJOUT] Ajout des bases du storage vuex.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,11 +5,17 @@ const routes = [
   {
     path: "/",
     name: "home",
+    meta: {
+      title: "Home",
+    },
     component: HomeView,
   },
   {
     path: "/about",
     name: "about",
+    meta: {
+      title: "About",
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -21,6 +27,23 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // e.g., if we have `/some/deep/nested/route` and `/some`, `/deep`, and `/nested` have titles,
+  // `/nested`'s will be chosen.
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find((r) => r.meta && r.meta.title);
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title;
+  }
+  next();
 });
 
 export default router;
