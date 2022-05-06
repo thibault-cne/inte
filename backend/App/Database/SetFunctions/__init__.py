910d4545f33dda4dd4070706d8dba877a434bdcf
Thib <thibault.cheneviere@telecomnancy.net>
2022-05-06 20:29:27 +0200

    [AJOUT] Ajout de package python pour l'architecture de backend.

---
+++ backend/App/Database/SetFunctions/__init__.py
@@ -0,0 +1,17 @@
+#  Copyright (c)
+#  Project : backend
+#
+#  --
+#
+#  Author : thibault
+#  File : __init__.py
+#  Description : *Enter description here*
+#
+#  --
+#
+#  Last modification : 2022/5/6
+
+# Import needed packages
+
+
+# Import personal packages
