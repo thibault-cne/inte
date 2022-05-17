8d729127becc76435d76efd0b3509cd5397ddd13
BigBaz <louisvincent@famillecapelli.fr>
2022-05-18 01:31:56 +0200

    [AJOUT] fonction qui formatte mieux les étoiles parce que c'était illisible

--- backend/App/Database/GetFunctions/getAllStars.py
+++ backend/App/Database/GetFunctions/getAllStars.py
@@ -37,6 +37,9 @@ def get_all_stars() -> list:
     # Close the database
     close_database(db, cursor)
 
+    # Change the format 
+    stars = [starMeilleurFormat(star) for star in stars]
+    
     # Return the result
     return stars
 
@@ -63,3 +66,15 @@ def get_all_stars_of_user(user_id: int) -> list:
 
     # Return the result
     return stars
+
+
+def starMeilleurFormat(star):
+    star1 = {}
+    star1["id"] = star[0]
+    star1["giver_user_id"] = star[1]
+    star1["receiver_user_id"] = star[2]
+    star1["star_rank"] = "d'or" if star[3] == 0 else "d'argent" if star[3] == 1 else "de bronze"
+    star1["message"] = star[4]
+    star1["date"] = star[5]
+    star1["moderation_status"] = star[6]
+    return star1
