f2e01eed837887ac490c7490da9c1fd3f2860e92
Thib <thibault.cheneviere@telecomnancy.net>
2022-10-01 18:16:43 +0200

    [ADD] Add vuetify 3 to the project.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { authStore } from "./store/authStore";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 
-createApp(App).use(authStore).use(router).mount("#app");
+loadFonts();
+
+createApp(App).use(router).use(vuetify).mount("#app");
