2b1b7d1591c01d57384d80ff48c871d97a9858f3
Thib <thibault.cheneviere@telecomnancy.net>
2022-10-19 08:52:46 +0200

    [+] models in frontend.

---
+++ frontend/src/models/User.ts
@@ -0,0 +1,22 @@
+type User = {
+    ID: string
+	Name: string
+	Email: string
+	Current_year: number
+	Promotion_year: number
+	Points: number
+	God_father_id: number
+	Facebook_id: string
+	Google_id: string
+	Instagram_id: string
+	Snapchat_id: string
+	Hometown: string
+	Studies: string
+	Personal_description: string
+	Profile_picture: string
+	Last_login: string
+	User_type: string
+	Color: string
+}
+
+export { User }
