e37bc6f4c27d1e2a097a07d3f787bdb73a10a341
Thib <thibault.cheneviere@telecomnancy.net>
2022-10-20 11:52:00 +0200

    [+] typeText component

---
+++ frontend/src/components/TypeText.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+    <span class="typed-text">{{ typeValue }}</span>
+    <span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "i-typeText",
+  props: {
+    message: { type: String, required: true },
+  },
+  data() {
+    return {
+      typeValue: "",
+      typeStatus: false,
+      typingSpeed: 75,
+      erasingSpeed: 100,
+      newTextDelay: 6000,
+      charIndex: 0,
+    };
+  },
+  created() {
+    setTimeout(this.typeText, 200);
+  },
+  methods: {
+    typeText() {
+      if (this.charIndex < this.message.length) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue += this.message.charAt(this.charIndex);
+        this.charIndex += 1;
+        setTimeout(this.typeText, this.typingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
+      }
+    },
+    eraseText() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue = this.message.substring(0, this.charIndex - 1);
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.typeText, this.typingSpeed + 1000);
+      }
+    },
+  },
+});
+</script>
+<style scoped lang="scss">
+@use "@/assets/styles/scss/standars/keyframes";
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  span.typed-text {
+    color: #d2b94b;
+  }
+  span.cursor {
+    display: inline-block;
+    margin-left: 3px;
+    width: 4px;
+    background-color: #fff;
+    animation: cursorBlink 1s infinite;
+  }
+
+  span.cursor.typing {
+    animation: none;
+  }
+}
+</style>
