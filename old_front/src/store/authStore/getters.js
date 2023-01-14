830257e464d68b765459089bd6b57da6e5bf2d62
thibault-cne <thibault.chene23@gmail.com>
2023-01-14 14:01:23 +0100

    [+] remove old front

--- old_front/src/store/authStore/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-  loggedIn: (state) => state.accessToken != null,
-  accessToken: (state) => state.accessToken,
-  refreshToken: (state) => state.refreshToken,
-};
-
-export { getters };
