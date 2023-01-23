c5405adeb937405c90a65751ac776634bad499e1
Yewolf <yyewolf@gmail.com>
2023-01-23 16:43:44 +0100

    [+] fix digit

--- frontend/src/components/digit.vue
+++ frontend/src/components/digit.vue
@@ -1,9 +1,9 @@
 <template>
-  <span class="single-digit">
     <span v-if="d == '.'" data-test="digit">.</span>
     <span v-else-if="d == ' '" data-test="digit">&nbsp;</span>
-    <span v-else :style="{ '--value': d }" data-test="digit"></span>
-  </span>
+    <span v-else class="single-digit">
+      <span :style="{ '--value': d }" data-test="digit"></span>
+    </span>
 </template>
 
 <script lang="ts">
